f7d2e8f48d4fb016dbaf55621bdc1b85a1a2b04f
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-03-13 23:31:30 +0100

    ft--updated cart view

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="container">
-        <h1>Your Shopping Cart {{ cartTotalItems ? `(${cartTotalItems})` : '' }}</h1>
+        <h1>Your Shopping Cart {{ cartTotalItems ? `(${cartTotalItems})` : 'is Empty' }}</h1>
         <div v-if="cartTotal === 0" class="empty">
             <span class="cart">
                 <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
             </span>
             <p class="noItems">You have no Items in your cart</p>
-            <button @click="() => $router.push('/products')">Discover Products</button>
+            <button @click="() => $router.push('/products')">Continue Shopping</button>
         </div>
         <div v-else class="table">
             <table>
