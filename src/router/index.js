f137ba455b30f56c28f0d84b05a104f8c1b7fbee
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-03-15 12:45:12 +0100

    ft--404 updated

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import store from '../store'
 
 // views
 import Home from '../views/Home.vue'
-import NotFound from '../views/NotFound.vue'
+
 
 
 // routes
@@ -56,7 +56,7 @@ const routes = [
   {
     name: 'NotFound',
     path: '/:pathMatch(.*)*',
-    component: NotFound,
+    component: () => import('@/views/NotFound.vue'),
   },
 ]
 
